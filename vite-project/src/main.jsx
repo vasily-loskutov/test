2f1d5834c8d686aa5c8d96feb82268bfb41cd961
vasily-loskutov <vasilijloskutov2083@gmail.com>
2023-02-15 15:55:39 +0300

    start project

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <h3>HEllo From Main</h3>
+    <h1>TEST HELLo</h1>
+  </React.StrictMode>
+);
