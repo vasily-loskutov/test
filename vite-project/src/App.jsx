9f0e1d91f788a2be201c0654f0e2c30a51bca0d0
vasily-loskutov <vasilijloskutov2083@gmail.com>
2023-02-15 15:59:14 +0300

    testComponent

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-
+import TestCompoment from "./testComponent";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <TestCompoment />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
---
+++ vite-project/src/testComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestCompoment = () => {
+  return <h1>TestCompoment</h1>;
+};
+
+export default TestCompoment;
